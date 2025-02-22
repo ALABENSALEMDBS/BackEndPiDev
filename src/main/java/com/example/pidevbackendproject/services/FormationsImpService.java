967a582dbd0fdb74cb1ff581cf6c0d27ef1b9b0c
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-02-22 23:06:55 +0100

    affectation joueur a formation et delete formation

--- src/main/java/com/example/pidevbackendproject/services/FormationsImpService.java
+++ src/main/java/com/example/pidevbackendproject/services/FormationsImpService.java
@@ -1,7 +1,10 @@
 package com.example.pidevbackendproject.services;
 
 import com.example.pidevbackendproject.entities.Formations;
+import com.example.pidevbackendproject.entities.Joueurs;
+import com.example.pidevbackendproject.entities.SousGroupes;
 import com.example.pidevbackendproject.repositories.FormationsRepo;
+import com.example.pidevbackendproject.repositories.JoueursRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +15,18 @@ import java.util.List;
 public class FormationsImpService implements IFormationsService {
     //******************
     FormationsRepo formationsRepo;
+    JoueursRepo joueursRepo;
     public Formations addFormations(Formations formation) {
         return formationsRepo.save(formation);
     }
 
-    public void deleteFormations(int idFormation) {
+    public void deleteFormations(int idFormation)
+    {
+        Formations formation = formationsRepo.findById(idFormation).get();
+        for(Joueurs joueur : formation.getJoueurs()) {
+            joueur.setFormation(null);
+            joueursRepo.save(joueur);
+        }
      formationsRepo.deleteById(idFormation);
     }
 
@@ -30,5 +40,15 @@ public class FormationsImpService implements IFormationsService {
 
     public Formations getFormationsById(int idFormation) {
         return formationsRepo.findById(idFormation).get();
+    }
+
+    public void affecterJoueurAFormation(int numjoueur, int idFormation) {
+        Joueurs joueur = joueursRepo.findByNumeroJoueur(numjoueur);
+        Formations formation = formationsRepo.getById(idFormation);
+        joueur.setFormation(formation);
+        if (formation.getJoueurs().size() < 11){
+            joueursRepo.save(joueur);
+        }else
+            throw new IllegalStateException("Le sous-groupe est déjà complet.");
     }
 }
