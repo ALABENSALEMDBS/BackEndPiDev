f3a874ba1c81989de82927d5384e95ee53cac85c
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-02-28 00:02:57 +0100

    ajouter fonction get joueurs by id traking

--- src/main/java/com/example/pidevbackendproject/services/FormationsImpService.java
+++ src/main/java/com/example/pidevbackendproject/services/FormationsImpService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -72,5 +73,11 @@ public class FormationsImpService implements IFormationsService {
         Formations formation = formationsRepo.getById(idFormation);
         tactic.setFormation(formation);
         tacticsRepo.save(tactic);
+    }
+
+    public Set<Joueurs> getJoueursInFormation(int idFormation) {
+        Formations formation = formationsRepo.findById(idFormation).get();
+
+        return formation.getJoueurs();
     }
 }
