a34657f8bf07e9774983f42ba87bb52250008d2d
OMARog0123 <omar.guesmi@esprit.tn>
2025-03-01 01:42:24 +0100

    rapport

--- src/main/java/com/example/pidevbackendproject/services/IRapportsService.java
+++ src/main/java/com/example/pidevbackendproject/services/IRapportsService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface IRapportsService {
     Rapports addRapports(Rapports rapport);
     void deleteRapports(int idRapport);
-    Rapports modifyRapports(Rapports rapport);
+    Rapports modifyRapports(int idRapport, Rapports rapport);
     List<Rapports> getAllRapports();
     Rapports getRapportsById(int idRapport);
 }
