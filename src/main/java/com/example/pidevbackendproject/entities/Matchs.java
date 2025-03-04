8f103a75c22c7efc6f92f82c81dde0b4e77816ea
rifakd <rifa.khadhraoui@esprit.tn>
2025-03-04 03:46:20 +0100

    rifaupdate

--- src/main/java/com/example/pidevbackendproject/entities/Matchs.java
+++ src/main/java/com/example/pidevbackendproject/entities/Matchs.java
@@ -17,8 +17,15 @@ import java.util.Set;
 public class Matchs {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int idMatch;
-    String resultatMatch;
+    private int idMatch;
+    private String resultatMatch;
+    private String dateMatch;  // Nouveau attribut pour la date du match
+    private String lieuMatch;  // Nouveau attribut pour le lieu du match
+    private String statusMatch; // Nouveau attribut pour l'état du match (par exemple "en cours", "terminé")
+    private String typeMatch;   // Nouveau attribut pour le type du match (par exemple "amical", "championnat")
+    private String arbitre;
+    private String equipe1;
+    private String equipe2;
 
 
     @JsonIgnore
