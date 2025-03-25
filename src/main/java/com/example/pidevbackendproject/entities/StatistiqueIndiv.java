f49f26db041524a68d9de9ea1bfb18582861e31c
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-03-25 23:05:41 +0100

    stat indiv

--- src/main/java/com/example/pidevbackendproject/entities/StatistiqueIndiv.java
+++ src/main/java/com/example/pidevbackendproject/entities/StatistiqueIndiv.java
@@ -15,8 +15,30 @@ public class StatistiqueIndiv {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int idStatistiqueIndiv;
-    Float vitesseStatistiqueIndiv;
-    String endurenceStatistiqueIndiv;
+
+    // Vitesse et endurance
+    Float vitesseStatistiqueIndiv;  // Vitesse du joueur
+    String endurenceStatistiqueIndiv; // Niveau d'endurance
+
+    // Statistiques offensives
+    int buts;  // Nombre de buts marqués
+    int passesDecisives; // Nombre de passes décisives
+    int tirs;  // Nombre de tirs tentés
+    int tirsCadres; // Nombre de tirs cadrés
+
+    // Statistiques défensives
+    int tacles;  // Nombre de tacles réussis
+    int fautesCommises; // Nombre de fautes commises
+    int cartonsJaunes; // Nombre de cartons jaunes reçus
+    int cartonsRouges; // Nombre de cartons rouges reçus
+
+    // Autres statistiques
+    int passesReussies; // Nombre de passes réussies
+    int dribblesReussis; // Nombre de dribbles réussis
+    int duelsGagnes; // Nombre de duels remportés
+    int distanceParcourue; // Distance totale parcourue en mètres
+
+
     @OneToOne(mappedBy = "statistiqueIndiv")
     Joueurs joueurstatistiqueIndiv;
 }
