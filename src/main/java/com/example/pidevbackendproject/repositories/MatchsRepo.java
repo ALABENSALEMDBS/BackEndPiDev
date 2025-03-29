3095a645e0227c4826868177d8b89b300a6eccf4
MaherKouki <koukimaher386@gmail.com>
2025-03-29 01:31:30 +0100

    goals and results

--- src/main/java/com/example/pidevbackendproject/repositories/MatchsRepo.java
+++ src/main/java/com/example/pidevbackendproject/repositories/MatchsRepo.java
@@ -2,8 +2,20 @@ package com.example.pidevbackendproject.repositories;
 
 import com.example.pidevbackendproject.entities.Matchs;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MatchsRepo extends JpaRepository<Matchs, Integer> {
+
+    //@Query("select p from Participant p join p.evenements ev join ev.logistiques log where p.tache='ORGANISATEUR' and log.reserve=false")
+
+
+    @Query("select m from Matchs m where m.resultatMatch=null ")
+    List<Matchs> notYetPlayedMatchs();
+
+    @Query("select m from Matchs m where m.resultatMatch!=null ")
+    List<Matchs> playedMatchs();
 }
